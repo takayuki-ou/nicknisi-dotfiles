ab97f90dc4a19b080200e36e4d6b10baad45e207
takoyakimantoman <uiwns625@gmail.com>
2024-12-31 00:01:21 +0900

    selection keymap changed

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,18 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   -- or                              , branch = '0.1.x',
+  opts = {
+    defaults = {
+      mappings = {
+        i = {
+          ["<C-j>"] = "move_selection_next",
+          ["<C-k>"] = "move_selection_previous"
+            -- actions.move_selection_next, type = "action",
+            -- opts = { nowait = true, silent = true }
+        }
+      }
+    }
+  },
   keys = {
   -- add a keymap to browse plugin files
   -- stylua: ignore
