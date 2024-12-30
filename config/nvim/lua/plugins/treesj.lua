1a8d97575c16edf951fef8731ad1fd00f2b445a7
takoyakimantoman <uiwns625@gmail.com>
2024-12-30 23:51:13 +0900

    config treesji for sf

---
+++ config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,31 @@
+return {
+    'Wansmer/treesj',
+    keys = { '<leader>m', '<leader>j', '<leader>s' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter
+    config = function()
+      require('treesj').setup({
+        ---@type boolean Use default keymaps (<leader>m - toggle, <leader>j - join, <leader>s - split)
+        use_default_keymaps = true,
+        ---@type boolean Node with syntax error will not be formatted
+        check_syntax_error = true,
+        ---If line after join will be longer than max value,
+        ---@type number If line after join will be longer than max value, node will not be formatted
+        max_join_length = 120,
+        ---Cursor behavior:
+        ---hold - cursor follows the node/place on which it was called
+        ---start - cursor jumps to the first symbol of the node being formatted
+        ---end - cursor jumps to the last symbol of the node being formatted
+        ---@type 'hold'|'start'|'end'
+        cursor_behavior = 'hold',
+        ---@type boolean Notify about possible problems or not
+        notify = true,
+        ---@type boolean Use `dot` for repeat action
+        dot_repeat = true,
+        ---@type nil|function Callback for treesj error handler. func (err_text, level, ...other_text)
+        on_error = nil,
+        ---@type table Presets for languages
+        -- langs = {}, -- See the default presets in lua/treesj/langs
+      })
+    end,
+  }
+
